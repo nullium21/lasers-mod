395a29d2eb9d43d35440c3fbd6c35103d5aa669c
Lina <dev@lina.moe>
2024-06-02 17:41:51 +0300

    rebrand to lasers

--- src/main/java/moe/lina/lasers/LasersMod.java
+++ src/main/java/moe/lina/lasers/LasersMod.java
@@ -1,15 +1,15 @@
-package com.example;
+package moe.lina.lasers;
 
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class LasersMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+    public static final Logger LOGGER = LoggerFactory.getLogger("lasers");
 
 	@Override
 	public void onInitialize() {

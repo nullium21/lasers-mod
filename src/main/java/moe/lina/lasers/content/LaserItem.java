88926850d5ba73531b2b2668125a332ca2f0f685
Lina <dev@lina.moe>
2024-06-04 10:08:50 +0300

    add a tooltip to laser item

--- src/main/java/moe/lina/lasers/content/LaserItem.java
+++ src/main/java/moe/lina/lasers/content/LaserItem.java
@@ -1,13 +1,18 @@
 package moe.lina.lasers.content;
 
 import moe.lina.lasers.LasersMod;
+import net.minecraft.client.item.TooltipType;
 import net.minecraft.component.DataComponentType;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+
+import java.util.List;
 
 import static moe.lina.lasers.LasersMod.id;
 
@@ -24,6 +29,16 @@ public class LaserItem extends BlockItem {
     public LaserItem() {
         super(LasersMod.LASER_BLOCK, new Settings()
                 .component(TARGETED_BLOCK, null));
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        super.appendTooltip(stack, context, tooltip, type);
+
+        var currentTarget = stack.get(TARGETED_BLOCK);
+
+        tooltip.add(Text.literal("Shift-RMB to point at a block"));
+        tooltip.add(Text.literal("Current target: ").append(currentTarget == null ? "---" : currentTarget.toShortString()));
     }
 
     @Override
